b2d47ea46799c0311dd1cdd1c7e2c086595c78d3
dashaBobrovad <dbobrova.dasha@gmail.com>
2025-05-19 19:25:52 +0300

    vlean project & counter

---
+++ src/components/MyHello.vue
@@ -0,0 +1,30 @@
+<!-- <script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
+};
+</script> -->
+
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+
+const increment = () => count.value++;
+
+// const updCount = (e) => (count.value = e.target.value);
+</script>
+
+<template>
+  <div>{{ count }}</div>
+  <button @click="increment">click</button>
+  <input @keyup.enter="increment" />
+</template>
