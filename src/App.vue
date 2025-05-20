3cd3ffe95ae376bddb5374df4fb5f262ddc4656a
dashaBobrovad <dbobrova.dasha@gmail.com>
2025-05-20 19:08:20 +0300

    add tailwind

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import MyHello from './components/MyHello.vue';
 </script>
 
 <template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
   <MyHello />
 </template>
 
--- src/components/MyHello.vue
+++ src/components/MyHello.vue
@@ -1,30 +1,3 @@
-<!-- <script>
-export default {
-  data() {
-    return {
-      count: 0,
-    };
-  },
-  methods: {
-    increment() {
-      this.count++;
-    },
-  },
-};
-</script> -->
+<script setup></script>
 
-<script setup>
-import { ref } from 'vue';
-
-const count = ref(0);
-
-const increment = () => count.value++;
-
-// const updCount = (e) => (count.value = e.target.value);
-</script>
-
-<template>
-  <div>{{ count }}</div>
-  <button @click="increment">click</button>
-  <input @keyup.enter="increment" />
-</template>
+<template></template>
