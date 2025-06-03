63515abd278651925ce8ebf2b36718a2e3fba5ea
dashaBobrovad <dbobrova.dasha@gmail.com>
2025-06-03 18:53:16 +0300

    router works

--- src/pages/Favorites.vue
+++ src/pages/Favorites.vue
@@ -1,1 +1,1 @@
-<template></template>
+<template><div>fav</div></template>
--- src/router.js
+++ src/router.js
@@ -1,13 +1,13 @@
-import { createWebHistory, createRouter } from 'vue-router';
+import { createWebHashHistory, createRouter } from 'vue-router';
 import Home from './pages/Home.vue';
 import Favorites from './pages/Favorites.vue';
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/fav', component: Favorites },
+  { path: '/', name: 'home', component: Home },
+  { path: '/fav', name: 'favorite', component: Favorites },
 ];
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
